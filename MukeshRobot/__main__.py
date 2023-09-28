cd0615534196e58f82aed77fe63a403527b1aa9f
ZoidsRobot <113953968+ZoidsRobot@users.noreply.github.com>
2023-09-29 02:38:50 +0700

    Update __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -215,7 +215,7 @@ def start(update: Update, context: CallbackContext):
             usr = update.effective_user
             lol = update.effective_message.reply_text(
                 PM_START_TEX.format(usr.first_name), parse_mode=ParseMode.MARKDOWN
-            
+            )
             update.effective_message.reply_photo(START_IMG,PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME,sql.num_users(),sql.num_chats()),
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
